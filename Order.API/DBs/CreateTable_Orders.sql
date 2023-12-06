757c7639e9a5f0e03abd32b3291255b3b936b3ce
oktamorph <oktamorph@gmail.com>
2023-12-06 11:17:46 +0100

    update the 'CreateTable_Orders.sql' script

--- Order.API/DBs/CreateTable_Orders.sql
+++ Order.API/DBs/CreateTable_Orders.sql
@@ -4,7 +4,10 @@ go
 create table Orders
 (
 	ID int identity(1, 1) not null primary key,
-	OrderGuid uniqueidentifier not null default newid(),
+	OrderGuid uniqueidentifier not null default newid() unique,
 	Description nvarchar(max) not null,
 	Price decimal(10, 2) not null
 );
+
+--select * from Orders;
+--drop table Orders;
